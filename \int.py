d25b24a0e861da1f34e9d7eaca6cfea72b8de293
Manuel Alejandro Goyo <31168052+ManuelAlejandrG@users.noreply.github.com>
2018-12-13 09:37:27 -0400

    Create \int.py

---
+++ \int.py
@@ -0,0 +1,12 @@
+from sympy import * 
+integrate(6*x**5, x)
+integrate(sin(x), x)
+
+integrate(log(x), x)
+
+integrate(2*x + sinh(x), x)
+
+
+integrate(exp(-x), (x, 0, oo))
+
+integrate(exp(-x**2), (x, -oo, oo))
