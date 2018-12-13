43940455aa3d7ba63bd9620f6962074fe0ab4640
Manuel Alejandro Goyo <31168052+ManuelAlejandrG@users.noreply.github.com>
2018-12-13 09:36:53 -0400

    Create diff.py

---
+++ diff.py
@@ -0,0 +1,14 @@
+from sympy import *
+diff(sin(x), x)
+cos(x)
+diff(sin(2*x), x)
+
+
+diff(tan(x), x)
+diff(sin(2*x), x, 1)  # diff(function, variable, orden)
+
+
+diff(sin(2*x), x, 2)
+
+
+diff(sin(2*x), x, 3)
